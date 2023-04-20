bf6aa49b90bc466a2ace5c112572b674b47610d1
Saeed beraia <saeed.beraia1996@gmail.com>
2023-04-20 14:24:13 +0300

    chat app init commit v1

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const port = 3001;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = [];
+const onConnected = (socket) => {
+  users = [...users, socket.id];
+
+  io.emit("online-member", users.length);
+  console.log("users", users);
+
+  socket.on("message", (message) => {
+    socket.broadcast.emit("emit-messages", message);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((id) => id !== socket.id);
+    io.emit("online-member", users.length);
+  });
+};
+
+io.on("connection", onConnected);
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
