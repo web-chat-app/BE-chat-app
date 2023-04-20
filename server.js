e654a06e7dfb324aabd4d2c1ba49027244d04618
Saeed beraia <saeed.beraia1996@gmail.com>
2023-04-20 15:51:31 +0300

    read port from env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 const io = new Server(server, {
